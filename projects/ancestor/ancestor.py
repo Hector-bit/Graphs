740d6bcd4769ec18592c598086439ae0a8ad7595
Hector <hectormartinez.2978@gmail.com>
2020-03-18 02:00:05 -0700

    naive solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,5 +1,23 @@
 from util import Stack, Queue
-
-def earliest_ancestor(ancestors, starting_node):
-    print(ancestors, 'ancestor')
-    print(starting_node, 'starting')
+"""
+Need to find a way to find the oldest ancestor of whatever number we pick,
+maybe I can do something similar to the order tickets problem on one of my sprints.
+"""
+def earliest_ancestor(ancestors, starting_node, valid_ancestor=None):
+    #some inputs will have multiple ancestors, pick smaller number when this happens
+    if valid_ancestor is None:
+        valid_ancestor = set()
+    #loop through ancestors
+    for i in ancestors:
+        print('pair', i)
+        #if starting node is in any of the tuples first index then...
+        if starting_node == i[1]: 
+            # recurse on the second number
+            earliest_ancestor(ancestors, i[0], valid_ancestor)
+            print(f'starting node {starting_node} is in this pair {i}, second number in tuple: {i[1]}')
+        else:
+            print('This is a valid ancestor: ', i[0])
+            valid_ancestor.add(i[0])
+            # if i[1] == starting_node:
+            #     return i[0]
+    print(valid_ancestor, 'ANCESTORS')
